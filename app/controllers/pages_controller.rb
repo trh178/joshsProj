b51685221a00a92cc6b508a189a990eb3045383c
trh178 <trh178@gmail.com>
2011-03-14 20:07:50 -0400

    added partials and title

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,32 +1,42 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def prob
+    @title = "Limbo"
   end
 
   def cat
+    @title = "Catalog"
   end
 
   def vote
+    @title = "Vote"
   end
 
   def buy
+    @title = "Purchase"
   end
 
   def about
+    @title = "About"
   end
 
   def contact
+    @title = "Contact"
   end
 
   def signin
+    @title = "Sign In"
   end
 
   def signup
+    @title = "Sign Up"
   end
 
   def blog
+    @title = "Blog"
   end
 
 end
